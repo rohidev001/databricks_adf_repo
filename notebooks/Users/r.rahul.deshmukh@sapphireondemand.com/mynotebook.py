1a8464cf4c805b276a4e7c194f3c3cba1638f405
rohidev001 <42436563+rohidev001@users.noreply.github.com>
2018-08-17 12:16:26 +0530

    adding notebook

---
+++ notebooks/Users/r.rahul.deshmukh@sapphireondemand.com/mynotebook.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+
+
+# COMMAND ----------
+
+# rest1.py
+# This script should demo databricks REST API.
+# ref:
+# goog: With python how to authenticate to REST API?
+# http://docs.python-requests.org/en/latest/
+import requests
+usr='r.rahul.deshmukh@sapphireondemand.com'
+pas='dapi748a8e2e3d9113f3e74463038cef972d'
+hhost         = 'https://abc-12345-911.cloud.databricks.com/api/1.2'
+clusters_list = hhost+'/clusters/list'
+rq1 = requests.get(clusters_list, auth=(usr,pas))
+print(rq1.status_code)
+print(rq1.json())
